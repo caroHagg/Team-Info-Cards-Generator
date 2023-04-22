ac718801bd47a8770fc5bdcb7014c03dc5480961
Carolina Haggerty <work@Carolinas-Mac.local>
2023-04-22 14:48:00 -0700

    added basic structure to employee,engineer,manager and inter js file

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Employee class
+class Employee{
+    constructor(name,employeeId,email){
+        this.name =name;
+        this.id =employeeId;
+        this.email = email;
+    }
+    getId(){
+
+    }
+    getName(){
+
+    }
+    getEmail(){
+
+    }
+    getRole(){
+        
+    }
+}
+module.exports = Employee;
